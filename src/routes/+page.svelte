1c479f510d1a157d64e7c080342a1ad0f6b9f725
Erik <erik.backstrom@gmail.com>
2024-03-21 12:28:32 +0100

    Trying to fix deploy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,2 @@
 <script>
-	import { Heading, Button, P, Skeleton, ImagePlaceholder, TextPlaceholder } from 'svelte-5-ui-lib';
-	import { ArrowRightOutline } from 'flowbite-svelte-icons';
-	import { Navbar, NavLi, NavBrand, NavUl, uiHelpers, Card } from 'svelte-5-ui-lib';
-	let nav = uiHelpers();
-
-	let navStatus = $state(false);
-	let toggleNav = nav.toggle;
-	let closeNav = nav.close;
-
-	$effect(() => {
-		// this can be done adding nav.navStatus directly to DOM element
-		// without using effect
-		navStatus = nav.isOpen;
-	});
 </script>

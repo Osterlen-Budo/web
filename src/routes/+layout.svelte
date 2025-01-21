d2ac291ad9f979a239f41d4f531fbabbd40c239c
Erik <erik.backstrom@gmail.com>
2025-01-22 00:31:18 +0100

    WIP

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,45 +1,86 @@
 <script>
 	import '../app.pcss';
 	import { page } from '$app/stores';
+	import { clickOutside } from '$lib/clickOutside';
 
-	import {Navbar, NavBrand, NavUl, NavLi, NavHamburger, DropdownItem, Dropdown} from 'flowbite-svelte'
-	import { ChevronDownOutline } from 'flowbite-svelte-icons';
+	import { Navbar, NavBrand } from 'flowbite-svelte';
 
-	import logo  from '$lib/assets/logo_no_text.png'
+	import logo from '$lib/assets/logo_no_text.png';
 	import { navigating } from '$app/stores';
-	let hideNavMenu = true
+	let hideNavMenu = true;
 
-	function toggleMenu () {
-		console.log("Hide menu")
+	let selected = '';
+
+	function toggleMenu() {
+		console.log('Hide menu');
 		hideNavMenu = false;
 	}
 
-
 	$: if ($navigating) {
-    	hideNavMenu = true
+		hideNavMenu = true;
 	}
-	$: activeUrl = $page.url.pathname
-	
+	$: activeUrl = $page.url.pathname;
+
+	const menuItems = [
+		{
+			title: 'Börja träna',
+			link: 'borjatrana'
+		},
+		{
+			title: 'Om Klubben',
+			link: 'omklubben'
+		},
+		{
+			title: 'Medlemsinfo',
+			sublinks: [
+				{ title: 'Ordningsregler', link: 'regler' },
+				{ title: 'Judopass', link: 'judopass' },
+				{ title: 'Försäkring', link: 'forsakring' }
+			]
+		},
+		{
+			title: 'Träning',
+			sublinks: [
+				{ title: 'Hur går träningen till', link: 'traningspass' },
+				{ title: 'Träningstider', link: 'tider' },
+				{ title: 'Avgifter', link: 'avgifter' },
+				{ title: 'Judons värdegrund', link: 'vardegrund' },
+				{ title: 'Gradering', link: 'gradering' },
+				{ title: 'Om tävling', link: 'tavla' }
+			]
+		}
+	];
+
+	function handleClickOutside() {
+		selected = '';
+	}
+
+	/**
+	 * @param {string} value
+	 */
+	function setSelected(value) {
+		selected = value === selected ? '' : value;
+	}
 </script>
 
-<div class="grid  sm:grid-cols-2 md:grid-cols-md lg:grid-cols-lg bg-white">
-	
-	<div class="fixed w-full t-0 left-0 sm:col-span-2 md:col-span-4 z-30">
-		
-		<Navbar class="border-b-2 t-0" >
+<div class="lg:grid-cols-lg grid bg-white sm:grid-cols-2 md:grid-cols-md">
+	<div class="t-0 absolute left-0 z-30 w-full sm:col-span-2 md:col-span-4">
+		<Navbar class="t-0 border-b-2">
 			<NavBrand href="/">
-			<img src={logo} class="me-3 h-6 sm:h-9" alt="Österlen Budo Logo" />
-			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Österlen Budoklubb</span>
+				<img src={logo} class="me-3 h-6 sm:h-9" alt="Österlen Budo Logo" />
+				<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+					>Österlen Budoklubb</span
+				>
 			</NavBrand>
-			<NavHamburger onClick={toggleMenu} />
+			<!--<NavHamburger onClick={toggleMenu} />
 			<NavUl {activeUrl} hidden={hideNavMenu}>
 				<NavLi href="/">Hem</NavLi>
 				<NavLi href="/borjatrana">Börja träna</NavLi>
 				<NavLi href="/kalendarium">Kalendarium</NavLi>
 				<NavLi class="cursor-pointer">
-					Träning<ChevronDownOutline class="w-6 h-6 ms-2 text-primary-800 dark:text-white inline" />
+					Träning<ChevronDownOutline class="ms-2 inline h-6 w-6 text-primary-800 dark:text-white" />
 				</NavLi>
-				<Dropdown class="w-44 z-20">
+				<Dropdown class="z-20 w-44">
 					<DropdownItem href="/traningspass">Hur går träning till</DropdownItem>
 					<DropdownItem href="/tider">Träningstider</DropdownItem>
 					<DropdownItem href="/avgifter">Avgifter</DropdownItem>
@@ -48,30 +89,71 @@
 					<DropdownItem href="/tavla">Om tävling</DropdownItem>
 				</Dropdown>
 				<NavLi class="cursor-pointer">
-					Medlemsinfo<ChevronDownOutline class="w-6 h-6 ms-2 text-primary-800 dark:text-white inline" />
+					Medlemsinfo<ChevronDownOutline
+						class="ms-2 inline h-6 w-6 text-primary-800 dark:text-white"
+					/>
 				</NavLi>
-				<Dropdown class="w-44 z-20">
+				<Dropdown class="z-20 w-44">
 					<DropdownItem href="/regler">Ordningsregler</DropdownItem>
 					<DropdownItem href="/judopass">Judopass</DropdownItem>
 					<DropdownItem href="/forsakring">Försäkringsinformation</DropdownItem>
 				</Dropdown>
 				<NavLi class="cursor-pointer">
-					Om klubben<ChevronDownOutline class="w-6 h-6 ms-2 text-primary-800 dark:text-white inline" />
+					Om klubben<ChevronDownOutline
+						class="ms-2 inline h-6 w-6 text-primary-800 dark:text-white"
+					/>
 				</NavLi>
-					<Dropdown class="w-44 z-20">
-						<DropdownItem href="/styrelse">Styrelse</DropdownItem>
-						<DropdownItem href="/tranare">Tränare</DropdownItem>
-						<!-- <DropdownItem href="/">Klubbens historia</DropdownItem> -->
-						<DropdownItem href="/dokument">Dokument</DropdownItem>
-
-					</Dropdown>
+				<Dropdown class="z-20 w-44">
+					<DropdownItem href="/styrelse">Styrelse</DropdownItem>
+					<DropdownItem href="/tranare">Tränare</DropdownItem>-->
+			<!-- <DropdownItem href="/">Klubbens historia</DropdownItem> -->
+			<!--<DropdownItem href="/dokument">Dokument</DropdownItem>
+				</Dropdown>
 				<NavLi href="/kontakt">Kontakt</NavLi>
-			</NavUl>
+			</NavUl>-->
 		</Navbar>
 	</div>
-	<slot/>
-	<div class="flex-col sm:col-span-2 sm:w-full md:col-span-1 md:col-start-3 md:mx-0" >
-		<!-- <div class="sm:w-full justify-self-start mb-10 mx-10">
+	<div class="flex w-screen justify-center">
+		<div
+			class="absolute mb-10 mt-20 grid grid-cols-2 flex-wrap justify-center overflow-y-visible sm:gap-4 md:w-8/12 md:grid-cols-4"
+		>
+			{#each menuItems as menu}
+				<div class="relative">
+					<a
+						href={menu.link}
+						on:click|preventDefault={() => setSelected(menu.title)}
+						class="w-42 flex h-12 items-center justify-center border-2 border-gray-100 bg-slate-100 text-center hover:bg-slate-100 sm:rounded-md"
+					>
+						<div class="text-md">{menu.title}</div>
+					</a>
+					{#if menu.sublinks}
+						<div
+							use:clickOutside
+							on:click_outside={handleClickOutside}
+							class="w-42 border-1 absolute z-0 col-span-8 {selected === `${menu.title}`
+								? ''
+								: 'hidden'} w-full bg-slate-100 shadow-2xl shadow-black"
+						>
+							{#each menu.sublinks as sub}
+								<div
+									class="border-2 border-slate-50 bg-slate-50 p-2 text-sm font-medium hover:bg-slate-200"
+								>
+									<a href={sub.link}>{sub.title}</a>
+								</div>
+								<!-- content here -->
+							{/each}
+						</div>
+
+						<!-- content here -->
+					{/if}
+				</div>
+			{/each}
+
+			<slot />
+		</div>
+	</div>
+	<!-- <div class="flex-col sm:col-span-2 sm:w-full md:col-span-1 md:col-start-3 md:mx-0" > -->
+	<!-- <div class="sm:w-full justify-self-start mb-10 mx-10">
 			<div class="">
 				<h5 class="mb-2 text-2xl font-thin border-b	tracking-tight text-gray-900 dark:text-white ">På gång</h5>
 				<b class="">April</b>
@@ -82,12 +164,12 @@
 				<p class="font-thin mb-2 text-gray-700 dark:text-gray-400 leading-tight">28/5 - Gradering</p>	
 			</div>
 		</div> -->
-		<div  class="sm:w-auto justify-center sm:justify-self-start mb-10 mx-10 sm:col-span-2">
-			<h5 class="mb-4 text-2xl font-thin border-b	 tracking-tight text-gray-900 dark:text-white">Sponsorer</h5>
+	<!-- <div  class="sm:w-auto justify-center sm:justify-self-start mb-10 mx-10 sm:col-span-2">
+			<h5 class="mb-4 text-2xl font-thin border-b	 tracking-tight text-gray-900 dark:teörxt-white">Sponsorer</h5>
 			<img src="/images/sponsor/Sjobo_logo.png" class="w-auto my-16 max-w-52" alt="Sjöbo"/>
 			<img  src="/images/sponsor/Sparbanken-skane-logo.png" class="w-auto my-16 max-w-52" alt="Sparbanken Skåne"/>
 			<img src="/images/sponsor/rf_sisu.svg" class="w-auto my-16 max-w-52" alt="RF-SISU"/>
 			<img src="/images/sponsor/sjobo_elnat.png" class="w-auto my-16 max-w-52" alt="Sjöbo Elnät"/>
-		</div>
-	</div>
+		</div> -->
+	<!-- </div> -->
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,58 +1,161 @@
 <script lang="ts">
-    import { NewsItem} from '$lib/components/index'
-    import logo  from '$lib/assets/logo_no_text.png'
-	
-    export let data
-    console.log("DaTA", data)
+	import { NewsItem } from '$lib/components/index';
+	import logo from '$lib/assets/logo_no_text.png';
 
+	export let data;
+	let children: any;
+	// }
 </script>
-<div class="text-center mt-28 flex items-center justify-center flex-col sm:col-span-2 md:col-span-4">
+
+<!-- <div class="text-center mt-28 flex items-center justify-center flex-col sm:col-span-2 md:col-span-4">
 	<img src={logo} alt="Österlen Budo Logo" width="200px" class="mb-5"/>
 	<p class="text-1xl font-normal md:text-2xl lg:text-2xl ">
         <span class=" px-2">Välkommen till Österlen Budoklubb</span> </p
 	>
 	<p class="mb-6 text-xs md:text-sm dark:text-gray-400 text-red-700 sm:px-12  xl:px-28"
 		>Den lilla klubben med det stora hjärtat</p>
+</div> -->
+<!-- svelte-ignore a11y-invalid-attribute -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+
+<!-- <div
+			class="w-42 flex h-12 items-center justify-center border-2 border-gray-100 bg-slate-100 text-center sm:rounded-md"
+		>
+			<div class="text-md">Börja träna</div>
+		</div>
+
+		<div
+			class="w-42 flex h-12 items-center justify-center border-2 border-gray-100 bg-slate-100 text-center sm:rounded-md"
+		>
+			<div class="text-md">Om klubben</div>
+		</div>
+		<div class="relative">
+			<div
+				on:click={() => setSelected('medlemsinfo')}
+				class="w-42 z-10 flex h-12 items-center justify-center border-2 border-gray-100 bg-slate-100 text-center {selected ===
+				'medlemsinfo'
+					? 'shadow-xl'
+					: ''} sm:rounded-md"
+			>
+				<div class="text-md">Medlemsinfo</div>
+			</div>
+			<div
+				class="w-42 border-1 absolute z-0 col-span-8 {selected === 'medlemsinfo'
+					? ''
+					: 'hidden'} w-full bg-slate-100 shadow-2xl shadow-black"
+			>
+				<div class="border-2 border-slate-50 bg-slate-50 p-2 text-sm font-medium">
+					Hur går träning till
+				</div>
+				<div class="bg-slate-5 border-2 border-slate-50 p-2 text-sm font-medium">
+					Ordningsregler
+				</div>
+				<div class="bg-slate-5 border-2 border-slate-50 p-2 text-sm font-medium">Judopass</div>
+				<div class="bg-slate-5 border-2 border-slate-50 p-2 text-sm font-medium">Försäkring</div>
+			</div>
+		</div>
+
+		<div>
+			<div
+				class="w-42 flex h-12 items-center justify-center border-2 border-gray-100 bg-slate-100 text-center sm:rounded-md"
+			>
+				<div class="text-md">Träning</div>
+			</div>
+			<div class="w-42 z-0 col-span-8 hidden w-full border-2">
+				<div class=" border-2 border-slate-50 bg-slate-50 p-2 text-sm font-medium">
+					Hur går träning till
+				</div>
+				<div class="bg-slate-5 border-2 border-slate-50 p-2 text-sm font-medium">Träningstider</div>
+				<div class="bg-slate-5 border-2 border-slate-50 p-2 text-sm font-medium">Avgifter</div>
+				<div class="bg-slate-5 border-2 border-slate-50 p-2 text-sm font-medium">Träningstider</div>
+				<div class="bg-slate-5 border-2 border-slate-50 p-2 text-sm font-medium">
+					Judons värdegrund
+				</div>
+				<div class="bg-slate-5 border-2 border-slate-50 p-2 text-sm font-medium">Gradering</div>
+				<div class="bg-slate-5 border-2 border-slate-50 p-2 text-sm font-medium">Om tävling</div>
+			</div>
+		</div> -->
+<div class=" col-span-2 h-12 pt-4 font-thin md:col-span-4">Våra grupper</div>
+<div class="flex h-52 justify-center">
+	<!-- <img src="/\images/assets/barn_nyborjare.png" /> -->
+	<div
+		class="flex h-52 w-48 items-center justify-center rounded-md border-2 border-white bg-[url('/images/assets/barn_nyborjare.png')] bg-cover bg-no-repeat shadow-lg"
+	>
+		<div
+			class="flex w-full items-center justify-center rounded-bl-2xl border-white bg-red-700 bg-opacity-75 p-2"
+		>
+			<span class="text-center text-lg text-white">Barn Nybörjare<br />7-12 år</span>
+		</div>
+	</div>
 </div>
 
-
-<div class=" m-5 flex w-screen mb-10 flex-wrap justify-center overflow-y-visible">
-    <div class="border-8 border-white bg-gray-50  px-2  h-52 rounded-lg m-4 bg-red-100 flex justify-center shadow-lg   ">
-        <div class="w-52">
-            <span class="text-xl text-black">Börja Träna</span>
-        </div>
-    </div>
-    
-    <div>
-        <div class="border-8 border-white bg-gray-50 h-24 w-24 rounded-lg px-2 mb-4  my-4  shadow-lg ">
-            <div class="">
-                <span class="text-sm ">Träningstider</span>
-            </div>
-            
-        </div>
-
-        <div class="border-8 border-white   bg-gray-50 w-24 h-24 px-2 my-4  rounded-lg shadow-lg ">
-            <div class="">
-                <span class="text-sm">Kalender</span>
-            </div>
-        </div>
-    </div>
-
-    <div class="border-8 border-white bg-gray-50 w-52 h-24   rounded-lg px-2  m-4 shadow-lg  ">
-        <span class="text-xl">Börja träna</span>
-    </div>
-
+<div class="flex h-52 justify-center">
+	<div
+		class="flex w-48 items-center justify-center rounded-md border-2 border-white bg-[url('/images/assets/barn_fortsattning.png')] bg-cover bg-no-repeat shadow-lg"
+	>
+		<div
+			class="flex w-full items-center justify-center rounded-bl-2xl border-white bg-red-700 bg-opacity-75 p-2"
+		>
+			<span class="text-center text-lg text-white">Barn Forts.<br />7-12 år</span>
+		</div>
+	</div>
 </div>
-<div class="text-left flex flex-row justify-center flex-wrap sm:col-span-2 md:col-start-2 md:col-span-1 md:justify-normal">
-    {#each data.newsItems as news}
+
+<div class="flex h-52 justify-center">
+	<div
+		class="flex w-48 items-center justify-center rounded-md border-2 border-white bg-[url('/images/assets/ungdomar.png')] bg-cover bg-no-repeat shadow-lg"
+	>
+		<div
+			class="flex w-full items-center justify-center rounded-bl-2xl border-white bg-red-700 bg-opacity-75 p-2"
+		>
+			<span class="text-center text-lg text-white">Ungdomar<br />13-17 år</span>
+		</div>
+	</div>
+</div>
+
+<div class="flex h-52 justify-center">
+	<div
+		class="flex w-48 items-center justify-center rounded-md border-2 border-white bg-[url('/images/assets/vuxen1.png')] bg-cover bg-no-repeat shadow-lg"
+	>
+		<div
+			class="flex w-full items-center justify-center rounded-bl-2xl border-white bg-red-700 bg-opacity-75 p-2"
+		>
+			<span class="text-center text-lg text-white">Vuxna<br />18+</span>
+		</div>
+	</div>
+</div>
+
+<div class="col-span-2 h-8 pt-4 font-thin md:col-span-4">Kalendarium</div>
+
+<div
+	class=" col-span-2 m-1 flex h-52 justify-start rounded-lg border-8 border-gray-100 px-2 shadow-lg"
+>
+	<div class="p-2">
+		<ul>
+			<o><span class="text-sm">30 Nov:</span><span class="text-base">Läger</span> </o>
+		</ul>
+	</div>
+</div>
+
+<div class="col-span-2 h-8 pt-4 font-thin md:col-span-4">Kontakt</div>
+
+<div
+	class=" col-span-2 m-1 flex h-52 justify-start rounded-lg border-8 border-gray-100 px-2 shadow-lg"
+>
+	<div class="mx-w-xs p-2">info@osterlenbudo.se</div>
+</div>
+
+<!-- <div class="text-left flex flex-row justify-center flex-wrap sm:col-span-2 md:col-start-2 md:col-span-1 md:justify-normal"> -->
+<!-- {#each data.newsItems as news}
         <NewsItem title={news.title} date={news.date} image={news.image || ""} slug={`nyheter/${news.slug}`} >
         </NewsItem>
-    {/each}
-    <!-- <div class="fb-page" data-href="https://www.facebook.com/osterlenbudo" data-tabs="timeline" data-width="" data-height="" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true">
+    {/each} -->
+<!-- <div class="fb-page" data-href="https://www.facebook.com/osterlenbudo" data-tabs="timeline" data-width="" data-height="" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true">
     <blockquote cite="https://www.facebook.com/osterlenbudo" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/osterlenbudo">Österlen Budo Klubb</a></blockquote></div>
  -->
 
-    <!--     
+<!--     
     <NewsItem title="Klubbtävling" pubDate="2024-03-31" image='/images/tavling.jpeg' slug="">
         Idag har vi haft en klubbtävling med 20 duktiga deltagare varav det var mångas första gång.
     </NewsItem>
@@ -65,4 +168,4 @@
     <NewsItem title="Påsklovsträning" pubDate="2024-03-31" image='/images/easter.jpeg' slug="" >
         Under påsklovet kan du komma och prova judo med oss. Inga förkunskaper krävs
     </NewsItem> -->
-</div>
+<!-- </div> -->

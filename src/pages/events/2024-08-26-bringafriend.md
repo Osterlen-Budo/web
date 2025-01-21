8f43f2c11b23ad763bd32d935e6a81020e9f1c2f
Erik <erik.backstrom@gmail.com>
2025-01-22 00:31:18 +0100

    WIP

---
+++ src/pages/events/2024-08-26-bringafriend.md
@@ -0,0 +1,10 @@
+---
+layout: event
+title: Bring a friend
+startDate: 2024-08-26
+endDate: 2024-08-29
+time: 9.30-10.00
+description: Ta med en kompis
+location: Dojon
+date: 2024-05-03T09:19:00.000Z
+---
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,15 @@
 <script>
 	import '../app.pcss';
 	import { page } from '$app/stores';
+	import { clickOutside } from '$lib/clickOutside';
 
-	import {
-		Navbar,
-		NavBrand,
-		NavUl,
-		NavLi,
-		NavHamburger,
-		DropdownItem,
-		Dropdown
-	} from 'flowbite-svelte';
-	import { ChevronDownOutline } from 'flowbite-svelte-icons';
+	import { Navbar, NavBrand } from 'flowbite-svelte';
 
 	import logo from '$lib/assets/logo_no_text.png';
 	import { navigating } from '$app/stores';
 	let hideNavMenu = true;
+
+	let selected = '';
 
 	function toggleMenu() {
 		console.log('Hide menu');
@@ -26,10 +20,51 @@
 		hideNavMenu = true;
 	}
 	$: activeUrl = $page.url.pathname;
+
+	const menuItems = [
+		{
+			title: 'Börja träna',
+			link: 'borjatrana'
+		},
+		{
+			title: 'Om Klubben',
+			link: 'omklubben'
+		},
+		{
+			title: 'Medlemsinfo',
+			sublinks: [
+				{ title: 'Ordningsregler', link: 'regler' },
+				{ title: 'Judopass', link: 'judopass' },
+				{ title: 'Försäkring', link: 'forsakring' }
+			]
+		},
+		{
+			title: 'Träning',
+			sublinks: [
+				{ title: 'Hur går träningen till', link: 'traningspass' },
+				{ title: 'Träningstider', link: 'tider' },
+				{ title: 'Avgifter', link: 'avgifter' },
+				{ title: 'Judons värdegrund', link: 'vardegrund' },
+				{ title: 'Gradering', link: 'gradering' },
+				{ title: 'Om tävling', link: 'tavla' }
+			]
+		}
+	];
+
+	function handleClickOutside() {
+		selected = '';
+	}
+
+	/**
+	 * @param {string} value
+	 */
+	function setSelected(value) {
+		selected = value === selected ? '' : value;
+	}
 </script>
 
 <div class="lg:grid-cols-lg grid bg-white sm:grid-cols-2 md:grid-cols-md">
-	<div class="t-0 fixed left-0 z-30 w-full sm:col-span-2 md:col-span-4">
+	<div class="t-0 absolute left-0 z-30 w-full sm:col-span-2 md:col-span-4">
 		<Navbar class="t-0 border-b-2">
 			<NavBrand href="/">
 				<img src={logo} class="me-3 h-6 sm:h-9" alt="Österlen Budo Logo" />
@@ -37,7 +72,7 @@
 					>Österlen Budoklubb</span
 				>
 			</NavBrand>
-			<NavHamburger onClick={toggleMenu} />
+			<!--<NavHamburger onClick={toggleMenu} />
 			<NavUl {activeUrl} hidden={hideNavMenu}>
 				<NavLi href="/">Hem</NavLi>
 				<NavLi href="/borjatrana">Börja träna</NavLi>
@@ -71,17 +106,55 @@
 				</NavLi>
 				<Dropdown class="z-20 w-44">
 					<DropdownItem href="/styrelse">Styrelse</DropdownItem>
-					<DropdownItem href="/tranare">Tränare</DropdownItem>
-					<DropdownItem href="/historia">Klubbens historia</DropdownItem>
-					<DropdownItem href="/dokument">Dokument</DropdownItem>
+					<DropdownItem href="/tranare">Tränare</DropdownItem>-->
+			<!-- <DropdownItem href="/">Klubbens historia</DropdownItem> -->
+			<!--<DropdownItem href="/dokument">Dokument</DropdownItem>
 				</Dropdown>
 				<NavLi href="/kontakt">Kontakt</NavLi>
-			</NavUl>
+			</NavUl>-->
 		</Navbar>
 	</div>
-	<slot />
-	<div class="flex-col sm:col-span-2 sm:w-full md:col-span-1 md:col-start-3 md:mx-0">
-		<!-- <div class="sm:w-full justify-self-start mb-10 mx-10">
+	<div class="flex w-screen justify-center">
+		<div
+			class="absolute mb-10 mt-20 grid grid-cols-2 flex-wrap justify-center overflow-y-visible sm:gap-4 md:w-8/12 md:grid-cols-4"
+		>
+			{#each menuItems as menu}
+				<div class="relative">
+					<a
+						href={menu.link}
+						on:click|preventDefault={() => setSelected(menu.title)}
+						class="w-42 flex h-12 items-center justify-center border-2 border-gray-100 bg-slate-100 text-center hover:bg-slate-100 sm:rounded-md"
+					>
+						<div class="text-md">{menu.title}</div>
+					</a>
+					{#if menu.sublinks}
+						<div
+							use:clickOutside
+							on:click_outside={handleClickOutside}
+							class="w-42 border-1 absolute z-0 col-span-8 {selected === `${menu.title}`
+								? ''
+								: 'hidden'} w-full bg-slate-100 shadow-2xl shadow-black"
+						>
+							{#each menu.sublinks as sub}
+								<div
+									class="border-2 border-slate-50 bg-slate-50 p-2 text-sm font-medium hover:bg-slate-200"
+								>
+									<a href={sub.link}>{sub.title}</a>
+								</div>
+								<!-- content here -->
+							{/each}
+						</div>
+
+						<!-- content here -->
+					{/if}
+				</div>
+			{/each}
+
+			<slot />
+		</div>
+	</div>
+	<!-- <div class="flex-col sm:col-span-2 sm:w-full md:col-span-1 md:col-start-3 md:mx-0" > -->
+	<!-- <div class="sm:w-full justify-self-start mb-10 mx-10">
 			<div class="">
 				<h5 class="mb-2 text-2xl font-thin border-b	tracking-tight text-gray-900 dark:text-white ">På gång</h5>
 				<b class="">April</b>
@@ -92,18 +165,12 @@
 				<p class="font-thin mb-2 text-gray-700 dark:text-gray-400 leading-tight">28/5 - Gradering</p>	
 			</div>
 		</div> -->
-		<div class="mx-10 mb-10 justify-center sm:col-span-2 sm:w-auto sm:justify-self-start">
-			<h5 class="mb-4 border-b text-2xl font-thin tracking-tight text-gray-900 dark:text-white">
-				Sponsorer
-			</h5>
-			<img src="/images/sponsor/Sjobo_logo.png" class="my-16 w-auto max-w-52" alt="Sjöbo" />
-			<img
-				src="/images/sponsor/Sparbanken-skane-logo.png"
-				class="my-16 w-auto max-w-52"
-				alt="Sparbanken Skåne"
-			/>
-			<img src="/images/sponsor/rf_sisu.svg" class="my-16 w-auto max-w-52" alt="RF-SISU" />
-			<img src="/images/sponsor/sjobo_elnat.png" class="my-16 w-auto max-w-52" alt="Sjöbo Elnät" />
-		</div>
-	</div>
+	<!-- <div  class="sm:w-auto justify-center sm:justify-self-start mb-10 mx-10 sm:col-span-2">
+			<h5 class="mb-4 text-2xl font-thin border-b	 tracking-tight text-gray-900 dark:teörxt-white">Sponsorer</h5>
+			<img src="/images/sponsor/Sjobo_logo.png" class="w-auto my-16 max-w-52" alt="Sjöbo"/>
+			<img  src="/images/sponsor/Sparbanken-skane-logo.png" class="w-auto my-16 max-w-52" alt="Sparbanken Skåne"/>
+			<img src="/images/sponsor/rf_sisu.svg" class="w-auto my-16 max-w-52" alt="RF-SISU"/>
+			<img src="/images/sponsor/sjobo_elnat.png" class="w-auto my-16 max-w-52" alt="Sjöbo Elnät"/>
+		</div> -->
+	<!-- </div> -->
 </div>
